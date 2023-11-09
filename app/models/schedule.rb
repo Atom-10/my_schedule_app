9fd10afa070b8b33c53efab55217a88b19760445
biot <gdwsura@yahoo.co.jp>
2023-11-09 17:27:21 +0900

    タイトル、開始日、終了日を必須項目に設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,6 @@
 class Schedule < ApplicationRecord
+  validates :title, length: {maximum: 20}, presence: true
+  validates :start, presence: true
+  validates :end, presence: true
+  validates :details, length: {maximum: 500}
 end
