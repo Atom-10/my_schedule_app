433dd731eaf98932066de6277f234e53acbd31b3
biot <gdwsura@yahoo.co.jp>
2023-11-09 21:04:06 +0900

    開始日より前に終了日を選択できないようにバリデーションを設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,5 +2,11 @@ class Schedule < ApplicationRecord
   validates :title, length: {maximum: 20}, presence: true
   validates :start, presence: true
   validates :end, presence: true
+  validate :start_end_check
   validates :details, length: {maximum: 500}
+
+  def start_end_check
+      errors.add(:end, "は開始日より後の日付を選択してください") unless
+      self.start <= self.end
+  end
 end
