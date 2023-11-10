b74b7e9ae6be8498e4251fcfd18fd35ba5e83063
biot <gdwsura@yahoo.co.jp>
2023-11-10 21:14:33 +0900

    モーダルテストボタンを実装　使いにくかったので新規スケジュールボタンを上に変更

---
+++ app/javascript/packs/modal.js
@@ -0,0 +1,18 @@
+const buttonOpen = document.getElementById("modalOpen");
+const buttonClose = document.querySelector(".modalClose");
+const modal = document.getElementById("modal");
+
+buttonOpen.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+buttonClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+document.addEventListener("click", (e) => {
+  // console.log(e.target);
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
