26beceed160abf6d8fa0ab6dd0ec57c1e1d75cbf
biot <gdwsura@yahoo.co.jp>
2023-11-13 17:45:13 +0900

    モーダルテストボタンを作成

---
+++ app/javascript/packs/modal.js
@@ -0,0 +1,17 @@
+const buttonOpen = document.getElementById("modalOpen");
+const buttonClose = document.querySelector(".modalClose");
+const modal = document.getElementById("modal");
+
+buttonOpen.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+buttonClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
